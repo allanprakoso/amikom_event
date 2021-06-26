128f861c7685e541640be3d81f3d6753a4885ac1
Allan <Allanprakoso7@gmail.com>
2021-06-26 14:39:53 +0700

    fix

--- src/handlers/adminHandler.js
+++ src/handlers/adminHandler.js
@@ -9,14 +9,13 @@ const countEvent = async (request, h) => {
     const Bootcamp = await connection.awaitQuery('SELECT count(id) as jml from events where idKategori=3');
     const allEvent = await connection.awaitQuery(`SELECT COUNT(id) as jml from events`)
     const countVisitor = await connection.awaitQuery(`
-    SELECT DISTINCT DATE_FORMAT(CAST(datetime as DATE), \"%a\") AS day , 
-    COUNT(ip) as visitor FROM \`visitor\` GROUP BY day ORDER BY day DESC LIMIT 7;`)
+    SELECT DISTINCT CAST(datetime AS DATE) AS datetime, DATE_FORMAT(CAST(datetime as DATE), \"%a\") AS day, COUNT(ip) as visitor FROM visitor GROUP BY datetime,day ORDER BY datetime DESC LIMIT 7;`)
 
     const countAll = {
-        "allEvent": {jumlah: allEvent[0].jml, persentase : 100},
-        "workshop": {jumlah: Workshop[0].jml, persentase : Number(Workshop[0].jml)/Number(allEvent[0].jml)*100},
-        "webinar":  {jumlah: Webinar[0].jml, persentase : Number(Webinar[0].jml)/Number(allEvent[0].jml)*100},
-        "bootcamp": {jumlah: Bootcamp[0].jml, persentase : Number(Bootcamp[0].jml)/Number(allEvent[0].jml)*100},
+        "allEvent": { jumlah: allEvent[0].jml, persentase: 100 },
+        "workshop": { jumlah: Workshop[0].jml, persentase: Number(Workshop[0].jml) / Number(allEvent[0].jml) * 100 },
+        "webinar": { jumlah: Webinar[0].jml, persentase: Number(Webinar[0].jml) / Number(allEvent[0].jml) * 100 },
+        "bootcamp": { jumlah: Bootcamp[0].jml, persentase: Number(Bootcamp[0].jml) / Number(allEvent[0].jml) * 100 },
         "visitors": countVisitor
     }
 
@@ -31,7 +30,7 @@ const getDataVisitor = async (request, h) => {
 }
 
 const postDataVisitor = async (request, h) => {
-    const { ip, country} = request.payload;
+    const { ip, country } = request.payload;
 
     const visitor = {
         ip: ip,
